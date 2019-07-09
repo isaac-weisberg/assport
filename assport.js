c07bdec792c59e7df520b37faf94db1eca377af4
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-07-09 11:08:11 +0300

    Add a brief piece of rendering

--- assport.js
+++ assport.js
@@ -1,7 +1,30 @@
+const root = document.getElementById('root')
+
 fetch('./assdata.json')
     .then(res => {
         return res.json()
     })
     .then(data => {
-        
+        for (const columnName in data) {
+            const cell = createCellWith(columnName)
+
+            const columnObject = document.createElement('div')
+            columnObject.className = 'column'
+            columnObject.appendChild(cell)
+            root.appendChild(columnObject)
+
+            for (const row in data[columnName]) {
+                const cell = createCellWith(data[columnName][row])
+                columnObject.appendChild(cell)
+            }
+        }
     })
+
+
+function createCellWith(content) {
+    const parent = document.createElement('div')
+    parent.className = 'cell'
+    const child = document.createTextNode(content)
+    parent.appendChild(child)
+    return parent
+}
